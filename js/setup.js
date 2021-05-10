99734702676b730951a40ed75eb0e6ba984d6e81
Yiling Hu <78616989+huyiling521@users.noreply.github.com>
2021-05-10 14:37:42 +0800

    data uploading

---
+++ js/setup.js
@@ -0,0 +1,18 @@
+var map = L.map('map', {
+  center: [39.96080665909226, -75.15987396240234],
+  zoom: 12,
+  zoomControl: false
+});
+
+L.control.zoom({
+  position: 'topright'
+}).addTo(map);
+
+var Stamen_TonerLite = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  subdomains: 'abcd',
+  minZoom: 0,
+  maxZoom: 20,
+  ext: 'png'
+}).addTo(map);
+
